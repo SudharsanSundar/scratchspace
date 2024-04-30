f560dc69b483f5ca63c8c83e686dbc798c1b72ac
SudharsanSundar <43732589+SudharsanSundar@users.noreply.github.com>
2024-04-30 14:16:24 -0700

    Update rmsnorm.py

--- rmsnorm.py
+++ rmsnorm.py
@@ -16,5 +16,24 @@ def RMSNorm_grad_wrt_x(grad_output: torch.Tensor, x: torch.Tensor, g: torch.Tens
     output_grad = add_term - final_mult_term
 
     final_grad = grad_output * output_grad
+
+    print(grad_output.shape, x.shape, g.shape, sum_eps.shape)
+    print(add_term.shape)
+    print(output_grad.shape)
+    print(final_grad.shape)
     
     return final_grad
+
+    # > pytest -k test_rmsnorm_backward_x_pytorch
+    #
+    # False = <built-in method allclose of type object at 0x14f7c1471840>(
+    # tensor([[[-6.4320e-03,  1.4323e-02,  5.9898e-02,  ...,  2.9904e-04,\n           6.8773e-02, -3.2441e-02],\n         [-1....1.3778e-01, -5.8441e-02, -3.1317e-01,  ..., -2.2864e-03,\n          -7.0831e-02, -1.5222e-01]]], grad_fn=<MulBackward0>),
+    # tensor([[[-3.8991e-02,  3.7541e-02,  5.9241e-02,  ...,  6.7614e-02,\n           4.9535e-02, -8.1552e-02],\n         [-1....1.8454e-01],\n         [-1.2955e-01, -6.2788e-02, -3.1281e-01,  ..., -5.3356e-03,\n          -7.2525e-02, -1.5264e-01]]])
+    # , rtol=0.0001, atol=1e-05)
+    #
+    # [STDOUT:
+    # torch.Size([10, 20, 50]) torch.Size([10, 20, 50]) torch.Size([50]) torch.Size([10, 20, 1])
+    # torch.Size([10, 20, 50])
+    # torch.Size([10, 20, 50])
+    # torch.Size([10, 20, 50])
+    # ]
